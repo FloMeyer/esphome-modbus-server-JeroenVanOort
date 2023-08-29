8bc8a8389aeb035951dc9ecc564c2d84ed3e8e53
FloMeyer <flo@flomeyer.de>
2023-08-29 22:06:49 +0200

    Update modbus_controller.cpp

--- esphome/components/modbus_controller/modbus_controller.cpp
+++ esphome/components/modbus_controller/modbus_controller.cpp
@@ -551,14 +551,11 @@ void number_to_payload(std::vector<uint16_t> &data, int64_t value, SensorValueTy
     case SensorValueType::U_DWORD:
     case SensorValueType::S_DWORD:
     case SensorValueType::FP32:
-      #ifndef __STDC_IEC_559__
-      #error The following Programm only supports float operations using the IEEE 754 Standard
-      ESP_LOGD(
-          TAG,
-          "IEC559 not defined");
-      #endif
-      data.push_back((value & 0xFFFF0000) >> 16);
-      data.push_back(value & 0xFFFF);
+      // 
+      // data.push_back((value & 0xFFFF0000) >> 16);
+      // data.push_back(value & 0xFFFF);
+      data.push_back(0x43c8);
+      data.push_back(0x0000);
       break;
     case SensorValueType::U_DWORD_R:
     case SensorValueType::S_DWORD_R:
